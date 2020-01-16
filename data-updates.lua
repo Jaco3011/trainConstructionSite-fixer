1ca9418dcfefcdb1d64dff54de4bc363316c160a
Jaco3011 <jacekhozej@gmail.com>
2020-01-16 19:38:39 +0100

    Add files via upload

--- data-updates.lua
+++ data-updates.lua
@@ -2,28 +2,33 @@
 local extraLayer1 = "layer-15" -- for rails
 local extraLayer2 = "layer-12" -- for belts
 
---add collision mask to the trainassembler itself
-
---the code was here
-
---add collision mask to curved rails
+--remove collision mask from curved rails
 for _, railData in pairs(data.raw["curved-rail"]) do
-  railData.collision_mask = 0
+  railData.collision_mask = util.table.deepcopy(railData.collision_mask or {"object-layer", "item-layer", "floor-layer", "water-tile"})
+    for key,value in pairs(railData.collision_mask) do
+        table.remove(railData.collision_mask, key)
+    end
 end
 
---add collision mask to signals
+--remove collision mask from signals
 for _, signalType in pairs({"rail-signal", "rail-chain-signal"}) do
   for _, signalData in pairs(data.raw[signalType]) do
-    signalData.collision_mask = 0
+    signalData.collision_mask = util.table.deepcopy(signalData.collision_mask or {"object-layer", "item-layer", "floor-layer", "water-tile"})
+    for key,value in pairs(signalData.collision_mask) do
+        table.remove(signalData.collision_mask, key)
+    end
   end
 end
 
---add collision mask to belts
+--remove collision mask from belts
 for _, beltType in pairs({
   "transport-belt",
 }) do
   for _, beltEntity in pairs(data.raw[beltType]) do
-    beltEntity.collision_mask = 0
+    beltEntity.collision_mask = util.table.deepcopy(beltEntity.collision_mask or {"floor-layer", "object-layer", "water-tile"})
+    for key,value in pairs(beltEntity.collision_mask) do
+        table.remove(beltEntity.collision_mask, key)
+    end
   end
 end
 for _, beltType in pairs({
@@ -31,6 +36,9 @@ for _, beltType in pairs({
   "splitter",
 }) do
   for _, beltEntity in pairs(data.raw[beltType]) do
-    beltEntity.collision_mask = 0
+    beltEntity.collision_mask = util.table.deepcopy(beltEntity.collision_mask or {"item-layer", "object-layer", "water-tile"})
+    or key,value in pairs(beltEntity.collision_mask) do
+        table.remove(beltEntity.collision_mask, key)
+    end
   end
 end
